73c3f0949d3da236a0afe0bd1064644bbcfe33dd
saparbayev-azizbek-01 <saparbayev.azizbek12@gmail.com>
2025-07-17 20:32:04 +0500

    new files uploaded

--- MLZone/tasks/urls.py
+++ MLZone/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import house_price, digit_recognition
+from .views import house_price, digit_recognition, spam_classifier
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('house-price/', house_price.index, name='house_price'),
     path('digit-recognition/', digit_recognition.index, name='digit_recognition'),
+    path('spam-classifier/', spam_classifier.index, name='spam_classifier'),
 ]
---
+++ lesson-8/practice.py
@@ -0,0 +1,14 @@
+people = [
+    {"name": "Alice", "degree": 2, "wealth_group": 3},
+    {"name": "Bob", "degree": 3, "wealth_group": 2},
+    {"name": "Charlie", "degree": 3, "wealth_group": 1},
+    {"name": "David", "degree": 2, "wealth_group": 1},
+    {"name": "Emma", "degree": 2, "wealth_group": 2},
+    {"name": "Frank", "degree": 1, "wealth_group": 1},
+    {"name": "Grace", "degree": 1, "wealth_group": 1},
+    {"name": "Henry", "degree": 1, "wealth_group": 1},
+    {"name": "Ivy", "degree": 2, "wealth_group": 2},
+    {"name": "Jack", "degree": 2, "wealth_group": 2},
+]
+
+total_people = len(people)
